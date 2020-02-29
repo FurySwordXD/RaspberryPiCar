a459c71d13cf0d6edb42f3e32319fe09ae4f3472
Sainath Ganesh <sainathganesh1@gmail.com>
2020-02-29 23:14:22 +0530

    Vehicle Controls

--- static/virtualjoystick.js
+++ static/virtualjoystick.js
@@ -1,227 +1,67 @@
+const joystick = createJoystick(document.getElementById('wrapper'));
 
-var INACTIVE = 0;
-var ACTIVE = 1;
-var SECONDS_INACTIVE = 0.5;
+// setInterval(() => console.log(joystick.getPosition()), 16);
 
-function loadSprite(src, callback) {
-    var sprite = new Image();
-    sprite.onload = callback;
-    sprite.src = src;
-    return sprite;
-}
+function createJoystick(parent) {
+    const maxDiff = 100;
+    const stick = document.createElement('div');
+    stick.classList.add('joystick');
 
+    stick.addEventListener('mousedown', handleMouseDown);
+    document.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener('mouseup', handleMouseUp);
+    stick.addEventListener('touchstart', handleMouseDown);
+    document.addEventListener('touchmove', handleMouseMove);
+    document.addEventListener('touchend', handleMouseUp);
 
-JoystickView = Backbone.View.extend({
-    events: {
-        "touchstart": "startControl",
-        "touchmove": "move",
-        "touchend": "endCotrol",
-        "mousedown": "startControl",
-        "mouseup": "endControl",
-        "mousemove": "move"
-    },
-    initialize: function(squareSize, finishedLoadCallback){
-        this.squareSize = squareSize;
-        this.template = _.template($("#joystick-view").html());
-        this.state = INACTIVE;
-        this.x = 0;
-        this.y = 0;
-        this.canvas = null;
-        this.context = null;
-        this.radius = (this.squareSize / 2) * 0.5;
+    let dragStart = null;
+    let currentPos = { x: 0, y: 0 };
 
-        this.finishedLoadCallback = finishedLoadCallback;
-
-        this.joyStickLoaded = false;
-        this.backgroundLoaded = false;
-        this.lastTouch = new Date().getTime();
-        self = this;
-        setTimeout(function(){
-            self._retractJoystickForInactivity();
-        }, 1000);
-        this.sprite = loadSprite("{{ url_for('static', filename='button.png') }}", function(){
-            self.joyStickLoaded = true;
-            self._tryCallback();
-        });
-        this.background = loadSprite("{{ url_for('static', filename='canvas.png') }}", function(){
-            self.backgroundLoaded = true;
-            self._tryCallback();
-        });
-    },
-    _retractJoystickForInactivity: function(){
-        var framesPerSec = 15;
-        var self = this;
-        setTimeout(function(){
-            var currentTime = new Date().getTime();
-            if(currentTime - self.lastTouch >= SECONDS_INACTIVE * 1000){
-                self._retractToMiddle();
-                self.renderSprite();
-            }
-            self._retractJoystickForInactivity();
-        }, parseInt(1000 / framesPerSec, 10));
-    },
-    _tryCallback: function(){
-        if(this.backgroundLoaded && this.joyStickLoaded){
-            var self = this;
-            this.finishedLoadCallback(self);
-        }
-    },
-    startControl: function(evt){
-        this.state = ACTIVE;
-    },
-    endControl: function(evt){
-        this.state = INACTIVE;
-        this.x = 0;
-        this.y = 0;
-        this.renderSprite();
-    },
-    move: function(evt){
-        if(this.state == INACTIVE){
+    function handleMouseDown(event) {
+        stick.style.transition = '0s';
+        if (event.changedTouches) {
+            dragStart = {
+                x: event.changedTouches[0].clientX,
+                y: event.changedTouches[0].clientY,
+            };
             return;
         }
-        this.lastTouch = new Date().getTime();
 
-        var x, y;
-
-
-        if(evt.originalEvent && evt.originalEvent.touches){
-            evt.preventDefault();
-            var left = 0;
-            var fromTop = 0;
-            elem = $(this.canvas)[0];
-            while(elem) {
-                left = left + parseInt(elem.offsetLeft);
-                fromTop = fromTop + parseInt(elem.offsetTop);
-                elem = elem.offsetParent;
-            }
-            x = evt.originalEvent.touches[0].clientX - left;
-            y = evt.originalEvent.touches[0].clientY - fromTop;
-        } else {
-            x = evt.offsetX;
-            y = evt.offsetY;
-        }
-        this._mutateToCartesian(x, y);
-        this._triggerChange();
-    },
-    _triggerChange: function(){
-        var xPercent = this.x / this.radius;
-        var yPercent = this.y / this.radius;
-        if(Math.abs(xPercent) > 1.0){
-            xPercent /= Math.abs(xPercent);
-        }
-        if(Math.abs(yPercent) > 1.0){
-            yPercent /= Math.abs(yPercent);
-        }
-        this.trigger("horizontalMove", xPercent);
-        this.trigger("verticalMove", yPercent);
-    },
-    _mutateToCartesian: function(x, y){
-        x -= (this.squareSize) / 2;
-        y *= -1;
-        y += (this.squareSize) / 2;
-        if(isNaN(y)){
-            y = this.squareSize / 2;
-        }
-
-        this.x = x;
-        this.y = y;
-        if(this._valuesExceedRadius(this.x, this.y)){
-            this._traceNewValues();
-        }
-        this.renderSprite();
-    },
-    _retractToMiddle: function(){
-        var percentLoss = 0.1;
-        var toKeep = 1.0 - percentLoss;
-
-        var xSign = 1;
-        var ySign = 1;
-
-        if(this.x != 0){
-            xSign = this.x / Math.abs(this.x);
-        }
-        if(this.y != 0) {
-            ySign = this.y / Math.abs(this.y);
-        }
-
-        this.x = Math.floor(toKeep * Math.abs(this.x)) * xSign;
-        this.y = Math.floor(toKeep * Math.abs(this.y)) * ySign;
-    },
-    _traceNewValues: function(){
-        var slope = this.y / this.x;
-        var xIncr = 1;
-        if(this.x < 0){
-            xIncr = -1;
-        }
-        for(var x=0; x<this.squareSize / 2; x+=xIncr){
-            var y = x * slope;
-            if(this._valuesExceedRadius(x, y)){
-                break;
-            }
-        }
-        this.x = x;
-        this.y = y;
-    },
-    _cartesianToCanvas: function(x, y){
-        var newX = x + this.squareSize / 2;
-        var newY = y - (this.squareSize / 2);
-        newY = newY * -1;
-        return {
-            x: newX,
-            y: newY
-        }
-    },
-    _valuesExceedRadius: function(x, y){
-        if(x === 0){
-            return y > this.radius;
-        }
-        return Math.pow(x, 2) + Math.pow(y, 2) > Math.pow(this.radius, 2);
-    },
-    renderSprite: function(){
-        var originalWidth = 89;
-        var originalHeight = 89;
-
-        var spriteWidth = 50;
-        var spriteHeight = 50;
-        var pixelsLeft = 0; //ofset for sprite on img
-        var pixelsTop = 0; //offset for sprite on img
-        var coords = this._cartesianToCanvas(this.x, this.y);
-        if(this.context == null){
-            return;
-        }
-        // hack dunno why I need the 2x
-        this.context.clearRect(0, 0, this.squareSize * 2, this.squareSize);
-
-        var backImageSize = 300;
-        this.context.drawImage(this.background,
-            0,
-            0,
-            backImageSize,
-            backImageSize,
-            0,
-            0,
-            this.squareSize,
-            this.squareSize
-        )
-        this.context.drawImage(this.sprite,
-            pixelsLeft,
-            pixelsTop,
-            originalWidth,
-            originalHeight,
-            coords.x - spriteWidth / 2,
-            coords.y - spriteHeight / 2,
-            spriteWidth,
-            spriteHeight
-        );
-    },
-    render: function(){
-        var renderData = {
-            squareSize: this.squareSize
+        dragStart = {
+            x: event.clientX,
+            y: event.clientY,
         };
-        this.$el.html(this.template(renderData));
-        this.canvas = this.$('#joystickCanvas')[0];
-        this.context = this.canvas.getContext('2d');
-        this.renderSprite();
-        return this;
+
     }
-});
+
+    function handleMouseMove(event) {
+        if (dragStart === null) return;
+        event.preventDefault();
+        if (event.changedTouches) {
+            event.clientX = event.changedTouches[0].clientX;
+            event.clientY = event.changedTouches[0].clientY;
+        }
+        const xDiff = event.clientX - dragStart.x;
+        const yDiff = event.clientY - dragStart.y;
+        const angle = Math.atan2(yDiff, xDiff);
+        const distance = Math.min(maxDiff, Math.hypot(xDiff, yDiff));
+        const xNew = distance * Math.cos(angle);
+        const yNew = distance * Math.sin(angle);
+        stick.style.transform = `translate3d(${xNew}px, ${yNew}px, 0px)`;
+        currentPos = { x: xNew, y: yNew };
+    }
+
+    function handleMouseUp(event) {
+        if (dragStart === null) return;
+        stick.style.transition = '.2s';
+        stick.style.transform = `translate3d(0px, 0px, 0px)`;
+        dragStart = null;
+        currentPos = { x: 0, y: 0 };
+    }
+
+    parent.appendChild(stick);
+    
+    return {
+        getPosition: () => currentPos,
+    };
+}
