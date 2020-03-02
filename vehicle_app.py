055435c7a050b00bc8e7ea87bcd724b556ea27f7
Sainath Ganesh <sainathganesh1@gmail.com>
2020-03-02 17:35:59 +0530

    Power ratio

--- vehicle_app.py
+++ vehicle_app.py
@@ -65,19 +65,20 @@ class RPICar:
         # GPIO.output(self.right_wheels_forward, GPIO.LOW)
         # GPIO.output(self.right_wheels_reverse, GPIO.LOW)
 
-        # if self.data['throttle'] > 0:
-        #     GPIO.output(self.left_wheels_reverse, GPIO.LOW)
-        #     GPIO.output(self.right_wheels_reverse, GPIO.LOW)
+        if self.data['throttle'] < 0.1:
+            GPIO.output(self.left_wheels_forward, GPIO.LOW)
+            GPIO.output(self.right_wheels_forward, GPIO.LOW)
 
-        #     GPIO.output(self.left_wheels_forward, GPIO.HIGH)
-        #     GPIO.output(self.right_wheels_forward, GPIO.HIGH)
+            GPIO.output(self.left_wheels_reverse, GPIO.HIGH)
+            GPIO.output(self.right_wheels_reverse, GPIO.HIGH)
 
-        # elif self.data['throttle'] < 0:
-        #     GPIO.output(self.left_wheels_forward, GPIO.LOW)
-        #     GPIO.output(self.right_wheels_forward, GPIO.LOW)
+        else:
 
-        #     GPIO.output(self.left_wheels_reverse, GPIO.HIGH)
-        #     GPIO.output(self.right_wheels_reverse, GPIO.HIGH)
+            GPIO.output(self.left_wheels_reverse, GPIO.LOW)
+            GPIO.output(self.right_wheels_reverse, GPIO.LOW)
+
+            GPIO.output(self.left_wheels_forward, GPIO.HIGH)
+            GPIO.output(self.right_wheels_forward, GPIO.HIGH)
 
         # if self.data['steer'] > 0:
         #     GPIO.output(self.left_wheels_forward, GPIO.HIGH)
