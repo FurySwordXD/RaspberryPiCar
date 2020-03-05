aef60f21a744d9d93169dd48d6b338400e7db317
Sainath Ganesh <sainathganesh1@gmail.com>
2020-03-05 19:06:58 +0530

    GA AI

--- vehicle_app.py
+++ vehicle_app.py
@@ -152,21 +152,28 @@ class RPICar:
     def calculate_distance(self, trigger, echo):
         maxTime = 0.04
 
-        GPIO.output(trigger,False)
+        GPIO.output(trigger, GPIO.LOW)
         time.sleep(0.01)
-        GPIO.output(trigger,True)
+        GPIO.output(trigger, GPIO.HIGH)
         time.sleep(0.00001)
-        GPIO.output(trigger,False)
+        GPIO.output(trigger, GPIO.LOW)
 
+        success = True
         pulse_start = time.time()
         timeout = pulse_start + maxTime
-        while GPIO.input(echo) == 0 and pulse_start < timeout:
-            pulse_start = time.time()
+        while GPIO.input(echo) == 0:
+            if pulse_start< timeout:
+                pulse_start = time.time()
+            else:
+                return 0
 
         pulse_end = time.time()
         timeout = pulse_end + maxTime
-        while GPIO.input(echo) == 1 and pulse_end < timeout:
-            pulse_end = time.time()
+        while GPIO.input(echo) == 1:
+            if pulse_end < timeout:
+                pulse_end = time.time()
+            else:
+                return 0
 
         pulse_duration = pulse_end - pulse_start
         distance = pulse_duration * 17000
